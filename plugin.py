38011a627d5889c7591aa1977f88067472bff692
F.K.Z <farooghkarimizadeh@gmail.com>
2017-04-26 23:49:36 +0430

    a little change, it doesn't effect execution
    i defined a new variable named MAX_DIFF and assigned 5 to it and replaced 5 with it, so someone who reads the code knows what this number(5) is for what purpose, it doesn't effect execution of the plug-in because 5 is 5 :)

--- plugin.py
+++ plugin.py
@@ -76,12 +76,14 @@ class qdata():
         self.ayahText = json["verse"]
 class utilities():
     def arg2list(ayat): # TODO: better name for this function
+        MAX_DIFF = 5 # This is the maximum difference between two verses number, or 
+                     # how many ayat user can get with a single command at most?
         try:
             return [int(ayat)]
         except ValueError:
             t = ayat.split('-') 
             start, end = int(t[0]), int(t[1])
-            if (end - start) not in range(1, 6):
+            if (end - start) not in range(1, MAX_DIFF + 1):
                 raise ValueError("Out of range(max is 5) or start ayah is" +
                 "bigger than end ayah.") #TODO: a better message please
             else:
